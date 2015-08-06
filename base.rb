d10382034aa22cf10cce751697cb95c44c65c59e
Bri in The Sky <brianh6854@googlemail.com>
2015-08-06 15:47:35 +0100

    Tutorial version

--- base.rb
+++ base.rb
@@ -6,7 +6,7 @@
 #
 # Application: base.rb
 #
-# Where it all happens
+# Where it all happens LinkedIn Example Application
 #
 ########################################################################
 
@@ -15,59 +15,48 @@ require 'rubygems' if RUBY_VERSION < "1.9"
 require 'rqrcode'
 require 'sinatra/base'
 
-SINATRADEBUG = 0
-
 # Yes i monkey patched Sinatra to add a get and post method
 
 class Sinatra::Base
 
   class << self
-  
+
     def getpost(path, opts={}, &block)
       get(path, opts, &block)
       post(path, opts, &block)
     end
 
-    def tea(path, opts={}, &block)
-      get(path, opts, &block)
-    end
-    
   end
-  
+
 end
 
 class QRCodeMaker < Sinatra::Base
 
   set :server, :puma
-  set :logging, :true if SINATRADEBUG == 1
-	
+  set :logging, :true
+
   configure do
     enable :session
-    if SINATRADEBUG == 1
-      Log = Logger.new("sinatra.log")
-      Log.level  = Logger::DEBUG 
-    end
   end
-    
+
   before do
-	content_type :html
+    content_type :html
   end
-	
+
   set :title, "QRCode Maker"
   set :port, 8080
-  
+  set :bind, "0.0.0.0"
+
   def qrcode(qrtext)
-	@qr = RQRCode::QRCode.new("#{qrtext}")
+    @qr = RQRCode::QRCode.new("#{qrtext}")
   end
-  
+
   getpost '/' do
-	if params[:submit] != nil
-	  @qrdata = qrcode("#{params[:qrtext]}")
-	end
-	
-    # Process ERB template render
+    if params[:submit] != nil
+      @qrdata = qrcode("#{params[:qrtext]}")
+    end
     puts "Process ERB"
-  	erb :base
+    erb :base
   end
   
   run!
